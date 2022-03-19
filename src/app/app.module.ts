662d8ce1cfb6fe757f47ab9d1aa8b58598b4a4d6
zthiagovalle <zthiagovalle@gmail.com>
2022-03-19 10:15:45 -0300

    criado projeto e primeiros componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './navegation/menu/menu.component';
+import { FooterComponent } from './navegation/footer/footer.component';
+import { HomeComponent } from './navegation/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
