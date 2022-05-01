1f1c6751485be78a48ddb72cebaa00fa2dc14a7c
zthiagovalle <zthiagovalle@gmail.com>
2022-05-01 11:57:39 -0300

    Adicionado placeholder e validacao de telefone no contatos

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,18 +8,21 @@ import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 })
 export class ContactComponent implements OnInit {
 
+  msgPlaceHolder: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.msgPlaceHolder = 'Olá, tenho uma sugestão de um ponto de coleta de lixo para fazer, na rua Brasil Salomão, número 341 está coletando pilhas e plásticos';
   }
 
   public sendEmail(e: Event) {
     e.preventDefault();
     emailjs.sendForm('service_zrezpv5', 'template_oz6rxq1', e.target as HTMLFormElement, 'CMgrFtG25XhOyPQKp')
       .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
+        alert('E-mail enviado com sucesso, muito obrigado')
       }, (error: any) => {
-        console.log(error.text);
+        alert('Falha ao enviar e-mail, envie novamente')
       });
   }
 }
