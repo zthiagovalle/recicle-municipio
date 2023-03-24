6095653d755c3d7a0afdabbdbb9b5799fbfd32fc
Thiago Valle <zthiagovalle@gmail.com>
2023-03-24 15:57:18 -0300

    att url endpoint

--- src/app/waste-card/service/waste.service.ts
+++ src/app/waste-card/service/waste.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Waste } from "../model/waste.model";
@@ -7,7 +7,7 @@ import { Waste } from "../model/waste.model";
 export class WasteService {
     constructor(private http: HttpClient) { }
 
-    protected url = "https://waste-api-prd-2.azurewebsites.net/waste";
+    protected url = "https://waste-api.onrender.com/";
 
     getWaste(): Observable<Waste[]> {
         return this.http.get<Waste[]>(this.url);
